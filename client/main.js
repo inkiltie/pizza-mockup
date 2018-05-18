a3c258cb67e5b0dfd55def17b66e63e8c3e8649e
Nikita Tolpekin <vancoder@Vancoder-MacBook-Pro.local>
2018-05-18 01:30:49 -0700

    Added all page components, stylized components, added responsiveness, added slick and increment/decrement functionality. Need to clean up the code and optimze performance

--- client/main.js
+++ client/main.js
@@ -3,6 +3,6 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import '/imports/startup/client';
 import '/imports/startup/both';
 
-import '/imports/ui/stylesheets/pizza.css';
+import '/imports/ui/stylesheets/pizza.min.css';
 
 BlazeLayout.setRoot('body');
